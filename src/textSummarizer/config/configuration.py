36276a9f4aa72c86c5a333ee9e57da6f220abd56
engineereliab076 <eliabujohnmpuya@gmail.com>
2025-01-29 02:04:52 +0300

    prediction and ui added

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -91,10 +91,10 @@ class ConfigurationManager:
           
           create_directories([config.root_dir])
 
-          model_trainer_config = ModelEvaluationConfig(
+          model_evaluation_config = ModelEvaluationConfig(
                 root_dir = config.root_dir,
                 data_path = config.data_path,
-                model_path = config.model_ckpt,
+                model_path = config.model_path,
                 tokenizer_path = config.tokenizer_path,
                 metric_file_name = config.metric_file_name
           )
